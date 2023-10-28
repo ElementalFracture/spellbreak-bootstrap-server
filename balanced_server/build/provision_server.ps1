f976c552ae82811614c17143b3c420f238656c14
Cory Finger <cory.j.finger@gmail.com>
2023-10-27 19:21:58 -0700

    Open Spellbreak ports. Add logging and comments

--- balanced_server/build/provision_server.ps1
+++ balanced_server/build/provision_server.ps1
@@ -1,1 +1,3 @@
+# Unzip Mod into g3\Content\Paks folder
+Write-Host "Unzipping mod to g3\\Content\\Paks..."
 Expand-Archive -LiteralPath "C:\\spellbreak-base-files\\balance-patch.zip" -DestinationPath "C:\\spellbreak-community-server\\g3\\Content\\Paks"
--- base_server/build/provision_server.ps1
+++ base_server/build/provision_server.ps1
@@ -1,11 +1,20 @@
+# Download Spellbreak Community Server files
+Write-Host "Downloading Spellbreak Community Server..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $serverc = New-Object System.Net.WebClient;
 $serverc.Headers.Add("User-Agent: Other");
 $serverc.DownloadFile("https://sbreak-comm-files.s3.amazonaws.com/spellbreak-community-version-server-windows+(1).zip", "C:\\spellbreak-base-files\\community-server.zip")
+Write-Host "Spellbreak Downloaded."
 
+# Unpack them to C:\spellbreak-community-server
+Write-Host "Unzipping server files..."
 Expand-Archive -LiteralPath "C:\\spellbreak-base-files\\community-server.zip" -DestinationPath "C:\\spellbreak-community-server"
 
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\spellbreak-community-server.lnk")
-$Shortcut.TargetPath = "C:\\spellbreak-community-server"
-$Shortcut.Save()
+# Make a shortcut on the desktop for easy access
+Write-Host "Creating convenient desktop shorcut..."
+New-Item -ItemType SymbolicLink -Target "C:\\spellbreak-community-server" -Path "$Home\Desktop\Spellbreak Community Server.lnk"
+
+# Open Port 7777 (Spellbreak's default port)
+Write-Host "Adding firewall rules to allow Spellbreak ports..."
+netsh advfirewall firewall add rule name="Spellbreak (TCP)" dir=in action=allow protocol=TCP localport=7777
+netsh advfirewall firewall add rule name="Spellbreak (UDP)" dir=in action=allow protocol=UDP localport=7777
