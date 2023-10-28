0cd2fbd483ba4e26a53855672dca621760c95d85
Cory Finger <cory.j.finger@gmail.com>
2023-10-28 01:05:07 -0700

    Fix startup script

--- balanced_server/build/provision_server.ps1
+++ balanced_server/build/provision_server.ps1
@@ -2,7 +2,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Add startup script
-Register-ScheduledTask -TaskName "SpellbreakSupervisor" -Trigger (New-ScheduledTaskTrigger -AtStartup) -Action (New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"C:\spellbreak-supervision\startup.ps1`"") -RunLevel Highest -Force -User "Administrator" -Password ""
+Register-ScheduledTask -TaskName "SpellbreakSupervisor" -Trigger (New-ScheduledTaskTrigger -AtStartup) -Action (New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"C:\spellbreak-supervision\startup.ps1`"") -RunLevel Highest -Force -User "Administrator" -Password "SuperSpellBS3cr3t!!!!"
 
 # Unzip Mod into g3\Content\Paks folder
 Write-Host "Unzipping mod to g3\\Content\\Paks..."
